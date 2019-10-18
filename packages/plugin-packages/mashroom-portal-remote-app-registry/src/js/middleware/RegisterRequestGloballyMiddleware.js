fdf4aa63c011ca84a3496a4edcde608d65caf776
Jürgen Kofler <juergen.kofler@nonblocking.at>
2019-10-18 15:14:30 +0200

    hint added

--- packages/plugin-packages/mashroom-portal-remote-app-registry/src/js/middleware/RegisterRequestGloballyMiddleware.js
+++ packages/plugin-packages/mashroom-portal-remote-app-registry/src/js/middleware/RegisterRequestGloballyMiddleware.js
@@ -15,6 +15,8 @@ export default class RegisterRequestGloballyMiddleware implements RegisterReques
 
     middleware() {
         return async (req: ExpressRequest, res: ExpressResponse, next: ExpressNextFunction) => {
+            // WARNING: The request will only be available for synchronous processing,
+            // otherwise we would have to use something like request-context
             globalRequestHolder.request = req;
             res.on('finish', () => {
                 globalRequestHolder.request = null;
