f013dfe6c56043cdd7383da0ba7cd8391ab4cccc
Jürgen Kofler <juergen.kofler@nonblocking.at>
2019-11-10 16:25:40 +0100

    throw an error if the Angular app is loaded more than once

--- packages/plugin-packages/mashroom-portal-demo-angular-app/src/main.ts
+++ packages/plugin-packages/mashroom-portal-demo-angular-app/src/main.ts
@@ -2,19 +2,26 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
-import {environment} from './environments/environment';
 
 import {PortalAppSetup, PortalClientServices} from './types';
 
-if (environment.production) {
+if (process.env.NODE_ENV === 'production') {
     enableProdMode();
 }
 
+let moduleCreated = false;
+
 const bootstrap = (element: HTMLElement, portalAppSetup: PortalAppSetup, portalClientServices: PortalClientServices) => {
+    if (moduleCreated) {
+        return Promise.reject('Due to framework limitations an Angular module can only be created once on a page!');
+    }
+
     element.innerHTML = `
         <angular-demo-app>
         </angular-demo-app>
     `;
+
+    moduleCreated = true;
 
     return platformBrowserDynamic([
         {provide: 'app.setup', useValue: portalAppSetup},
