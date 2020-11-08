e51b0da93456bcdc5e754471b8a8111c8c618715
Kofler Jürgen <juergen.kofler@nonblocking.at>
2020-11-08 20:19:47 +0100

    adaptions to webpack 5

--- packages/plugin-packages/mashroom-portal/src/frontend/welcome-app/js/template.js
+++ packages/plugin-packages/mashroom-portal/src/frontend/welcome-app/js/template.js
@@ -2,13 +2,13 @@
 
 import logoImg from '../assets/mashroom_logo.png';
 
-export default (resourcesBasePath: string) => `
+export default () => `
     <div class="mashroom-welcome-portal-app">
         <h4>Welcome to the Mashroom Portal Server!</h4>
         <div class="welcome-app-content">
-            <img src="${resourcesBasePath}/${logoImg}"/>
+            <img src="${logoImg}"/>
             <p>
-                This demonstration portal page integrates multiple standalone (single page) apps. 
+                This demonstration portal page integrates multiple standalone (single page) apps.
                 <br/>
                 All of them are written in different frontend technologies.
                 <br/>
