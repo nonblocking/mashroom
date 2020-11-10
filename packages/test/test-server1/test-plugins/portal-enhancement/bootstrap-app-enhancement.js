4977a0ee4c249fbd269f3a4106f12fc085abbae1
Kofler Jürgen <juergen.kofler@nonblocking.at>
2020-11-10 17:27:41 +0100

    only enhance a specific app

--- packages/test/test-server1/test-plugins/portal-enhancement/bootstrap-app-enhancement.js
+++ packages/test/test-server1/test-plugins/portal-enhancement/bootstrap-app-enhancement.js
@@ -2,6 +2,10 @@
 const bootstrap = async () => {
     return {
         enhancePortalAppSetup: (portalAppSetup, portalApp, req) => {
+            if (portalApp.name !== 'Portal App Enhancement Test App') {
+                return Promise.resolve(portalAppSetup);
+            }
+
             const logger = req.pluginContext.loggerFactory('test.portal.enhancer');
 
             logger.info(`===== Enhancing portalAppSetup: ${JSON.stringify(portalAppSetup, null, 2)} =====`);
