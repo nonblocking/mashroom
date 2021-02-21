c42bbff7024a39479241bd02cde2a9436f698e2f
Kofler Jürgen <juergen.kofler@nonblocking.at>
2021-02-21 17:16:17 +0100

    background jobs metrics added

---
+++ packages/plugin-packages/mashroom-background-jobs/src/js/metrics/background_jobs_metrics.ts
@@ -0,0 +1,28 @@
+
+import type {MashroomPluginContextHolder} from '@mashroom/mashroom/type-definitions';
+import type {MashroomMonitoringMetricsCollectorService} from '@mashroom/mashroom-monitoring-metrics-collector/type-definitions';
+import type {MashroomBackgroundJobService} from '../../../type-definitions';
+
+const EXPORT_INTERVAL_MS = 5000;
+
+let interval: NodeJS.Timeout;
+
+export const startExportBackgroundJobMetrics = (backgroundJobService: MashroomBackgroundJobService, pluginContextHolder: MashroomPluginContextHolder) => {
+    interval = setInterval(async () => {
+        const pluginContext = pluginContextHolder.getPluginContext();
+        const collectorService: MashroomMonitoringMetricsCollectorService = pluginContext.services.metrics && pluginContext.services.metrics.service;
+
+        if (collectorService) {
+            const jobsTotal = backgroundJobService.jobs.length;
+            const jobsFailed = backgroundJobService.jobs.filter((j) => j.lastInvocation && !j.lastInvocation.success).length;
+
+            collectorService.gauge('mashroom_background_jobs_total', 'Mashroom Background Jobs Total').set(jobsTotal);
+            collectorService.gauge('mashroom_background_jobs_failed', 'Mashroom Background Jobs In Failed State').set(jobsFailed);
+        }
+
+    }, EXPORT_INTERVAL_MS);
+}
+
+export const stopExportBackgroundJobMetrics = () => {
+    clearInterval(interval);
+};
--- packages/plugin-packages/mashroom-background-jobs/src/js/services/mashroom-bootstrap-services.ts
+++ packages/plugin-packages/mashroom-background-jobs/src/js/services/mashroom-bootstrap-services.ts
@@ -1,10 +1,20 @@
 
 import MashroomBackgroundJobService from './MashroomBackgroundJobService';
+import {
+    startExportBackgroundJobMetrics,
+    stopExportBackgroundJobMetrics
+} from '../metrics/background_jobs_metrics';
 
 import type {MashroomServicesPluginBootstrapFunction} from '@mashroom/mashroom/type-definitions';
 
 const bootstrap: MashroomServicesPluginBootstrapFunction = async (pluginName, pluginConfig, pluginContextHolder) => {
+    const pluginContext = pluginContextHolder.getPluginContext();
     const service = new MashroomBackgroundJobService(pluginContextHolder);
+
+    startExportBackgroundJobMetrics(service, pluginContextHolder);
+    pluginContext.services.core.pluginService.onUnloadOnce(pluginName, () => {
+        stopExportBackgroundJobMetrics();
+    });
 
     return {
         service,
