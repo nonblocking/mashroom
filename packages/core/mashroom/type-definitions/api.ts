a4811c5773fad594984ca8161e90d8f77b053659
Kofler Jürgen <juergen.kofler@nonblocking.at>
2021-06-08 20:43:46 +0200

    style fix

--- packages/core/mashroom/type-definitions/api.ts
+++ packages/core/mashroom/type-definitions/api.ts
@@ -57,9 +57,7 @@ export type ExpressMiddleware = RequestHandler;
 
 export type I18NString =
     | string
-    | {
-    [lang: string]: string;
-};
+    | { [lang: string]: string; };
 
 /**
  * Generic event emitter (under the hood node's EventEmitter will be used)
