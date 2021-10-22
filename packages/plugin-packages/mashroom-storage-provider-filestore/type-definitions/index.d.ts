0135fd01f41308a95d9092ee5450baee5c0890e0
Kofler Jürgen <juergen.kofler@nonblocking.at>
2021-10-22 17:50:42 +0200

    import fixed

--- packages/plugin-packages/mashroom-storage-provider-filestore/type-definitions/index.d.ts
+++ packages/plugin-packages/mashroom-storage-provider-filestore/type-definitions/index.d.ts
@@ -1,6 +1,5 @@
 
-import type {StorageObject} from '@mashroom/mashroom-storage/type-definitions';
-import {MashroomStorageCollection} from '@mashroom/mashroom-storage/type-definitions';
+import type {StorageObject, MashroomStorageCollection} from '@mashroom/mashroom-storage/type-definitions';
 
 export type CollectionMap = {
     [name: string]: MashroomStorageCollection<any>;
