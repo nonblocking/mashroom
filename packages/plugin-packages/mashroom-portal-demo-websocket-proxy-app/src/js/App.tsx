254175e8d5e4b64bcf456ebe7c6f9a5ccb2f44f9
David Strohmaier <davidst2017@yahoo.com>
2022-03-19 08:16:35 +0100

    Reconnect after server closed the connection

--- packages/plugin-packages/mashroom-portal-demo-websocket-proxy-app/src/js/App.tsx
+++ packages/plugin-packages/mashroom-portal-demo-websocket-proxy-app/src/js/App.tsx
@@ -1,50 +1,62 @@
-
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 
 type Props = {
     echoWSPath: string;
-}
+};
 
 type State = {
-    status: 'CONNECTING' | 'OPEN' | 'CLOSED' | 'ERROR';
+    status: 'CONNECTING' | 'OPEN' | 'CLOSED' | 'ERROR' | 'RECONNECTING';
     error: string | null;
     receivedMessage: string | null;
     inputMessage: string;
-}
+};
+
+const MAX_NUM_RETRIES = 5;
 
 export default class App extends PureComponent<Props, State> {
+    client?: WebSocket | null;
+    wsUrl: string;
+    retries: number;
 
-    client: WebSocket;
-
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            status: 'CONNECTING',
-            error: null,
-            receivedMessage: null,
-            inputMessage: '',
-        };
-
-        let wsUrl = props.echoWSPath;
+    fixWsUrl(wsUrl: string): string {
         if (wsUrl.indexOf('://') === -1) {
-            const socketProtocol = (global.location.protocol === 'https:' ? 'wss:' : 'ws:');
+            const socketProtocol =
+                global.location.protocol === 'https:' ? 'wss:' : 'ws:';
             const host = global.document.location.hostname;
-            const port = global.document.location.port ? `:${global.document.location.port}` : '';
-            wsUrl = `${socketProtocol}//${host}${port}${props.echoWSPath}`;
+            const port = global.document.location.port
+                ? `:${global.document.location.port}`
+                : '';
+            return `${socketProtocol}//${host}${port}${wsUrl}`;
         }
+        return wsUrl;
+    }
 
-        this.client = new WebSocket(wsUrl);
+    connect() {
+        this.client = new WebSocket(this.wsUrl);
         this.client.onopen = () => {
             console.info('Connection opened');
             this.setState({
                 status: 'OPEN',
             });
+            this.retries = 0;
         };
         this.client.onclose = () => {
-            console.error('Connection closed:');
-            this.setState({
-                status: 'CLOSED',
-            });
+            this.client = null;
+            setTimeout(() => {
+                this.connect();
+            }, 1000);
+            if (this.retries < MAX_NUM_RETRIES) {
+                console.error('Connection closed, reconnecting...');
+                this.setState({
+                    status: 'RECONNECTING',
+                });
+                this.retries += 1;
+            } else {
+                console.error('Connection closed for good');
+                this.setState({
+                    status: 'CLOSED',
+                });
+            }
         };
         this.client.onerror = (error) => {
             console.error('Error:', error);
@@ -61,14 +73,33 @@ export default class App extends PureComponent<Props, State> {
         };
     }
 
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            status: 'CONNECTING',
+            error: null,
+            receivedMessage: null,
+            inputMessage: '',
+        };
+        this.retries = MAX_NUM_RETRIES;
+
+        this.wsUrl = this.fixWsUrl(props.echoWSPath);
+
+        this.connect();
+    }
+
     onChange(inputMessage: string) {
         this.setState({
             inputMessage,
-        })
+        });
     }
 
     onSend() {
-        const {inputMessage} = this.state;
+        if (!this.client) {
+            console.error('Error sending faild. Client undefined.');
+            return;
+        }
+        const { inputMessage } = this.state;
         if (!inputMessage.trim()) {
             return;
         }
@@ -80,41 +111,41 @@ export default class App extends PureComponent<Props, State> {
     }
 
     render() {
-        const {status, error, inputMessage, receivedMessage} = this.state;
+        const { status, error, inputMessage, receivedMessage } = this.state;
 
         return (
-            <div className='mashroom-demo-websocket-proxy-app'>
+            <div className="mashroom-demo-websocket-proxy-app">
                 {status === 'CONNECTING' && (
-                    <div className='connecting'>
-                        Connecting...
-                    </div>
+                    <div className="connecting">Connecting...</div>
+                )}
+                {status === 'RECONNECTING' && (
+                    <div className="connecting">Reconnecting...</div>
                 )}
                 {status === 'ERROR' && (
-                    <div className='error'>
-                        Error: {error}
-                    </div>
+                    <div className="error">Error: {error}</div>
                 )}
                 {status === 'CLOSED' && (
-                    <div className='error'>
-                        Error: Connection closed
-                    </div>
+                    <div className="error">Error: Connection closed</div>
                 )}
                 {status === 'OPEN' && (
                     <div className="echo">
                         <div className="info">
                             <span className="info-icon" />
-                            This App connects via the Mashroom HTTP Proxy to a WebSocket echo server
+                            This App connects via the Mashroom HTTP Proxy to a
+                            WebSocket echo server
                         </div>
                         <div className="input-form">
-                            <input type="text" value={inputMessage} onChange={(e) => this.onChange(e.target.value)} />
+                            <input
+                                type="text"
+                                value={inputMessage}
+                                onChange={(e) => this.onChange(e.target.value)}
+                            />
                             <button onClick={() => this.onSend()}>Send</button>
                         </div>
                         {receivedMessage && (
                             <div className="received-message">
                                 Received:
-                                <pre>
-                                    {receivedMessage}
-                                </pre>
+                                <pre>{receivedMessage}</pre>
                             </div>
                         )}
                     </div>
@@ -122,5 +153,4 @@ export default class App extends PureComponent<Props, State> {
             </div>
         );
     }
-
 }
