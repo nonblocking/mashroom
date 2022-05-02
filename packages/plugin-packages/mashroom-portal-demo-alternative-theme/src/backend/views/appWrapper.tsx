3a37dfb737d7e75e96d7945f00f4d4fa1f3c57b9
Jürgen Kofler <juergen.kofler@nonblocking.at>
2022-05-02 16:00:39 +0200

    added example appWrapper and appError template

---
+++ packages/plugin-packages/mashroom-portal-demo-alternative-theme/src/backend/views/appWrapper.tsx
@@ -0,0 +1,18 @@
+
+// This template could safely be removed, since it resembles the default
+
+import React from 'react';
+import type {MashroomPortalAppWrapperRenderModel} from '@mashroom/mashroom-portal/type-definitions';
+
+export default ({ appId, safePluginName, appSSRHtml, title }: MashroomPortalAppWrapperRenderModel) => (
+    <div data-mr-app-id={appId} className={`mashroom-portal-app-wrapper portal-app-${safePluginName}`}>
+        <div className="mashroom-portal-app-header">
+            <div data-mr-app-content="title" className="mashroom-portal-app-header-title">{title}</div>
+        </div>
+        <div data-mr-app-content="app" className="mashroom-portal-app-host">
+            {appSSRHtml && <div dangerouslySetInnerHTML={{ __html: appSSRHtml }} />}
+            {!appSSRHtml && <div className="mashroom-portal-app-loading"><span/></div>}
+       </div>
+    </div>
+);
+
