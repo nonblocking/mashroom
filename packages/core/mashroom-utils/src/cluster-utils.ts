4eed9a9e45963188bff7f05e14000fdda8702402
Jürgen Kofler <juergen.kofler@nonblocking.at>
2023-12-19 17:02:09 +0100

    refactoring

--- packages/core/mashroom-utils/src/cluster-utils.ts
+++ packages/core/mashroom-utils/src/cluster-utils.ts
@@ -10,25 +10,21 @@ export const getWorkerId = (): string => {
     return process.env.NODE_APP_INSTANCE || (cluster.worker && String(cluster.worker.id)) || 'master';
 };
 
-export const getAllWorkerPids = (): Promise<Array<number>> => {
-    return findProcess('pid', process.pid).then(
-        ([thisProcess]) => {
-            if (!thisProcess || !thisProcess.ppid) {
-                return [];
-            }
-            return findProcess('name', thisProcess.name).then(
-                (workers) => {
-                    if (!workers) {
-                        return [];
-                    }
-                    return workers
-                        .filter((worker) => worker.ppid === thisProcess.ppid)
-                        .map((worker) => worker.pid);
-                }
-            );
+export const getAllWorkerPids = async (): Promise<Array<number>> => {
+    try {
+        const [thisProcess] = await findProcess('pid', process.pid);
+        if (!thisProcess || !thisProcess.ppid) {
+            return [];
         }
-    ).catch(() => {
+        const workers = await findProcess('name', thisProcess.name);
+        if (!workers) {
+            return [];
+        }
+        return workers
+            .filter((worker) => worker.ppid === thisProcess.ppid)
+            .map((worker) => worker.pid);
+    } catch (e) {
         // Ignore error
-        return Promise.resolve([]);
-    });
+        return [];
+    }
 };
--- packages/core/mashroom-utils/test/cluster-utils.test.ts
+++ packages/core/mashroom-utils/test/cluster-utils.test.ts
@@ -12,18 +12,14 @@ describe('cluster_utils.isNodeCluster', () => {
 
 describe('cluster_utils.getAllWorkerPids', () => {
 
-    it('returns a single pid when no cluster active', (done) => {
+    it('returns a single pid when no cluster active', async () => {
         if (process.env.GITHUB_WORKFLOW === 'Mashroom Run Tests Windows') {
             // TODO: find out why it doesn't work on windows server while it works on a local windows
-            done();
-        } else {
-            getAllWorkerPids().then(
-                (pids) => {
-                    expect(pids.length).toBeGreaterThan(0);
-                    done();
-                }
-            );
+            return;
         }
+
+        const pids = await getAllWorkerPids();
+        expect(pids.length).toBeGreaterThan(0);
     });
 
 });
