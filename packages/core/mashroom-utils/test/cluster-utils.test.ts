692fe5659f1b374103ed3b79d96ffdc5075d0105
Jürgen Kofler <juergen.kofler@nonblocking.at>
2024-07-01 10:45:36 +0200

    unit test fix

--- packages/core/mashroom-utils/test/cluster-utils.test.ts
+++ packages/core/mashroom-utils/test/cluster-utils.test.ts
@@ -13,7 +13,7 @@ describe('cluster-utils.isNodeCluster', () => {
 describe('cluster-utils.getAllWorkerPids', () => {
 
     it('returns a single pid when no cluster active', async () => {
-        if (process.env.GITHUB_WORKFLOW === 'Mashroom Run Tests Windows') {
+        if (process.env.GITHUB_WORKFLOW === 'Unit Tests Windows') {
             // TODO: find out why it doesn't work on windows server while it works on a local windows
             return;
         }
