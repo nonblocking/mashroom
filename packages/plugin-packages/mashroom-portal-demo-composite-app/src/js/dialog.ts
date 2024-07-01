a38ac616d4d9847b3d18858de242e00d5fc89366
Jürgen Kofler <juergen.kofler@nonblocking.at>
2024-07-01 18:08:13 +0200

    typos fixed

--- packages/plugin-packages/mashroom-portal-demo-composite-app/src/js/dialog.ts
+++ packages/plugin-packages/mashroom-portal-demo-composite-app/src/js/dialog.ts
@@ -3,21 +3,21 @@ export default [
     {
         name: 'Mashroom Portal Demo Vue App',
         appConfig: {
-            message: 'This simple Vue SPA that acts as the first page',
+            message: 'A simple Vue SPA that acts as the first page',
             pingButtonLabel: 'Next'
         }
     },
     {
         name: 'Mashroom Portal Demo Angular App',
         appConfig: {
-            message: 'This simple Angular SPA that acts as the second page',
+            message: 'A simple Angular SPA that acts as the second page',
             pingButtonLabel: 'Next'
         }
     },
     {
         name: 'Mashroom Portal Demo Svelte App',
         appConfig: {
-            message: 'This simple Svelte SPA that acts as the third and last page',
+            message: 'A simple Svelte SPA that acts as the third and last page',
             pingButtonLabel: 'Start over'
         }
     }
