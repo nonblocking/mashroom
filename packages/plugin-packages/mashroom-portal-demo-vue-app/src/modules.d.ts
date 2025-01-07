d86bb7d27ae2564bc171b2afcc5ab9b1a907d79b
Jürgen Kofler <juergen.kofler@nonblocking.at>
2025-01-07 17:55:01 +0100

    fixed

--- packages/plugin-packages/mashroom-portal-demo-vue-app/src/modules.d.ts
+++ packages/plugin-packages/mashroom-portal-demo-vue-app/src/modules.d.ts
@@ -1,4 +1,5 @@
 declare module '*.vue' {
-    import type { Component } from 'vue';
-    export default Component;
+    import type { DefineComponent } from 'vue';
+    const component: DefineComponent<unknown, unknown, any>;
+    export default component;
 }
